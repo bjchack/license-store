0e4388f496d35d002bd5ac40cc6cae88b177edce
bjchack <bjchack@gmail.com>
2025-12-15 18:08:35 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,1 @@
-(function(){
-    const p = ["c2Vj","cmV0","MTIz"]; // sec + ret + 123
 
-    function build(){
-        return atob(p[0]) + atob(p[1]) + atob(p[2]);
-    }
-
-    window.checkPassword = function(input){
-        return input === build();
-    };
-})();
