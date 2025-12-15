1636f717229394d9bd7f60d98a7c50047043e15b
bjchack <bjchack@gmail.com>
2025-12-15 18:36:35 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,13 +6,8 @@
     // PASSWORD 2 = bjc2025
     const p2 = "YmpjMjAyNQ=="; // bjc2025
 
-    function build1(){
-        return atob(p1[0]) + atob(p1[1]) + atob(p1[2]);
-    }
-
-    function build2(){
-        return atob(p2);
-    }
+    function build1(){ return atob(p1[0]) + atob(p1[1]) + atob(p1[2]); }
+    function build2(){ return atob(p2); }
 
     window.checkPassword = function(input){
         return input === build1() || input === build2();
