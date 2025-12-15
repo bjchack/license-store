0138b1f20fbcee24e0b53966832314e922a50739
bjchack <bjchack@gmail.com>
2025-12-15 17:58:48 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,18 @@
-// simple obfuscated password system
 (function(){
 
-    const encoded = [
+    // PASSWORD = secret123
+    const data = [
         "c2Vj", // sec
         "cmV0", // ret
         "MTIz"  // 123
     ];
 
-    function getPass(){
-        return atob(encoded[0]) + atob(encoded[1]) + atob(encoded[2]);
+    function build(){
+        return atob(data[0]) + atob(data[1]) + atob(data[2]);
     }
 
     window.checkPassword = function(input){
-        return input === getPass();
-    }
+        return input === build();
+    };
 
 })();
